4e8fdbab5d00e67433b6b19c743132ee10c4c43b
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-17 15:01:26 +0300

    config: configure deployment to netlfiy

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ let url;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   url = "http://localhost:5000";
 } else {
-  url = "production_url";
+  url = "https://codepile-backend.onrender.com";
 }
 
 export { url };
